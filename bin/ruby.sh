adf4030e575007118b0405864153883f2babc7ff
Tommy Bishop <bishop.thomas@gmail.com>
2011-06-06 13:25:41 -0700

    add aliases for bundler

--- bin/ruby.sh
+++ bin/ruby.sh
@@ -3,6 +3,8 @@
 export RUBYOPT="-rubygems"
 export r="rails"
 alias rautotest='RSPEC=true autotest'
+alias b="bundle"
+alias be="bundle exec"
 
 # rvm
 [[ -s $HOME/.rvm/scripts/rvm ]] && source $HOME/.rvm/scripts/rvm
