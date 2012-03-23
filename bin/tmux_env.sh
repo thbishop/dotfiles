28641eed42e935f210e6246a151cf232199b3913
Tommy Bishop <bishop.thomas@gmail.com>
2012-03-23 16:01:45 -0700

    add tmux_env ls command

--- bin/tmux_env.sh
+++ bin/tmux_env.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 TMUX_ENV_HOME=~/bin/tmux_envs
-TMUX_ENV_FILE=$TMUX_ENV_HOME/$1.sh
 
-if [ -x "$TMUX_ENV_FILE" ]; then
-  $TMUX_ENV_FILE
+if [ $1 == 'ls' ]; then
+  for x in `ls $TMUX_ENV_HOME | sort`; do
+    echo "${x%%.*}";
+  done
 else
-  echo "Unable to find a tmux environment at $TMUX_ENV_FILE"
+  TMUX_ENV_FILE=$TMUX_ENV_HOME/$1.sh
+
+  if [ -x "$TMUX_ENV_FILE" ]; then
+    $TMUX_ENV_FILE
+  else
+    echo "Unable to find a tmux environment at $TMUX_ENV_FILE"
+  fi
 fi
