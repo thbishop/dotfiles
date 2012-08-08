183ebc6603e6e98622ab6329cf26513e4c9b6227
Tommy Bishop <bishop.thomas@gmail.com>
2012-08-08 16:06:30 -0700

    add git aliases

--- bin/git.sh
+++ bin/git.sh
@@ -6,5 +6,8 @@ alias gba="git branch -a"
 alias gst="git status"
 alias gd="git diff"
 alias gds="git diff --staged"
+alias gf="git fetch"
+alias gfp="git fetch --prune"
 alias glogdiff="git log -p"
 alias glp='git log --graph --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset"'
+alias grm="git rebase origin/master"
