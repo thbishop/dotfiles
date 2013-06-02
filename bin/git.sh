2313cae2147b4c391056dddbd9db38cf7e0658e2
Tommy Bishop <bishop.thomas@gmail.com>
2013-06-01 23:51:37 -0700

    add git alias for grabbing the latest commit

--- bin/git.sh
+++ bin/git.sh
@@ -9,6 +9,7 @@ alias gds="git diff --staged"
 alias gf="git fetch"
 alias gfp="git fetch --prune"
 alias glogdiff="git log -p"
+alias glc="git rev-parse HEAD | tr '\n' ' ' | pbcopy"
 alias glp='git log --graph --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset"'
 alias grm="git rebase origin/master"
 alias gundo="git reset --soft HEAD^"
