3d3a90bcaa6773469752b2104a9b52f413cb8fcc
Tommy Bishop <tommy@thbishop.com>
2021-06-01 13:33:53 -0700

    Update pyenv init.

--- bin/shell.sh
+++ bin/shell.sh
@@ -49,7 +49,9 @@ export NVM_DIR="$HOME/.nvm"
 
 eval "$(rbenv init -)"
 
-eval "$(pyenv init -)"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
 eval "$(pyenv virtualenv-init -)"
 
 export PATH="$HOME/.jenv/bin:$PATH"
