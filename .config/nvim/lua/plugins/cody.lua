773744e1627efbe994f235a6229456411b5e0f2c
Tommy Bishop <tommy@thbishop.com>
2024-07-02 16:04:30 -0700

    nvim: add cody

---
+++ .config/nvim/lua/plugins/cody.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"sourcegraph/sg.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		config = function()
+			require("sg").setup({})
+		end,
+	},
+}
