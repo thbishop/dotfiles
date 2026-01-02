dd29274322025ab0b15652d520b8d7ce461adfc8
Tommy Bishop <tommy@thbishop.com>
2026-01-02 15:51:02 -0800

    nvim: fix cmp border

--- .config/nvim/lua/user/plugins/cmp.lua
+++ .config/nvim/lua/user/plugins/cmp.lua
@@ -22,8 +22,8 @@ return {
 					end,
 				},
 				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
+					completion = cmp.config.window.bordered({ border = "rounded" }),
+					documentation = cmp.config.window.bordered({ border = "rounded" }),
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-n>"] = cmp.mapping.select_next_item(),
